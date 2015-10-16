2240f329bc765a3347e748d7ba4c173c7dfc2b1f
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2015-10-17 01:19:22 +0200

    vadere obstacles become walls in unity
    working on #3: looping through the obstacles array in json and create a
    wall for each in the 3D scene, calculating the four polygon points from
    the x, y, width and height attributes in a vadere obstacle

--- Assets/Helper/FileLoaderJSON.cs
+++ Assets/Helper/FileLoaderJSON.cs
@@ -1,20 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using SimpleJSON;
 
 public class FileLoaderJSON {
 
 	public void loadJSONFile(string filename) {
+
+		// set scence
+		GeometryLoader gl = GameObject.Find("GeometryLoader").GetComponent<GeometryLoader>();
+		gl.setTheme (new NatureThemingMode ());
+
 	
 		string jsonStr = System.IO.File.ReadAllText (filename);
 
 		JSONNode rootnode = JSON.Parse(jsonStr);
-		JSONArray arr = rootnode["vadere"]["topography"]["obstacles"].AsArray;
+		JSONArray obstacles = rootnode["vadere"]["topography"]["obstacles"].AsArray;
 
-		for (int i = 0; i < arr.Count; i++) {
-			Debug.Log (arr[i]);
+
+		for (int i = 0; i < obstacles.Count; i++) {
+			JSONNode obstacle = obstacles[i];
+			Debug.Log (obstacle);
+			float height = 4;
+			ObstacleExtrudeGeometry.create("wall", parsePoints(obstacle), height);
 		}
 
+	
+	}
+
+
+	static List<Vector2> parsePoints(JSONNode obstacle) {
+		List<Vector2> list = new List<Vector2>();
+
+		var x = obstacle["shape"]["x"].AsFloat;
+		var y = obstacle["shape"]["y"].AsFloat;
+		var width = obstacle["shape"]["width"].AsFloat;
+		var height = obstacle["shape"]["height"].AsFloat;
+
+		list.Add(new Vector2(x, y));
+		list.Add(new Vector2(x + width, y));
+		list.Add(new Vector2(x + width, y + height));
+		list.Add(new Vector2(x, y + height));
+
+		return list;
 	}
 
 }
