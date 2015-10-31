2d08f1a36ede2a5ef5abe8e18cd38e53e98e0d83
Christos Tsiliakis <@>
2015-11-01 00:55:13 +0100

    Camera is now able to move in the direction in which the users looks.

---
+++ Assets/Walk.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walk : MonoBehaviour {
+
+	CardboardHead head = null;
+
+	//determine if the camera moves or not
+	private bool isMoving = false;
+
+	//This is the variable for the movement speed
+	[Tooltip("Speed of the camera movement.")]
+	public float speed = 2f;
+
+	[Tooltip("Activate if the height of the camera should be fixed.")]
+	public bool fixedHeight; 
+
+	[Tooltip("The height (y-coordinate) of the camera.")]
+	public float yOffset = 1.55f;
+
+	// Use this for initialization
+	void Start () {
+	
+		head = Camera.main.GetComponent<StereoController>().Head;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//turn the movement on or off with a trigger/rap
+		if (Cardboard.SDK.Triggered) {
+			
+			isMoving = !isMoving;
+		}
+
+		if (isMoving){
+			Vector3 direction = new Vector3 (head.transform.forward.x, 0, head.transform.forward.z).normalized * speed * Time.deltaTime;
+			Quaternion rotation = Quaternion.Euler (new Vector3 (0, -transform.rotation.eulerAngles.y, 0));
+			transform.Translate (rotation * direction);
+
+			if(fixedHeight)
+			{
+				transform.position = new Vector3(transform.position.x, yOffset, transform.position.z);
+			}
+		}
+
+
+	}
+}
+	
+	
