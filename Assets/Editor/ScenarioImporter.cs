590cd9e6ae95f7fcc7be9817ea6b6ae5e17de883
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2015-11-05 22:59:55 +0100

    configure import flow
    after using the new menu item „Assets/Import Vader Scenario“ user can
    save modified scene before continuing, gets warned if new scene would
    override existing one

--- Assets/Editor/ScenarioImporter.cs
+++ Assets/Editor/ScenarioImporter.cs
@@ -1,31 +1,34 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using System.IO;
 using System.Collections;
 
 public class ScenarioImporter : MonoBehaviour {
 
 	[MenuItem("Assets/Import Vadere Scenario")]
 
-	static void ImportVadereScenario() {		
-		EditorApplication.NewScene();
+	static void ImportVadereScenario() {	
 
-		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.transform.Rotate(0, 100, 0);
-		cube.transform.localScale = new Vector3(1.0f, 1.0f, 4.0f);
-		
-		EditorApplication.SaveScene("Assets/Scenes/NewScene.unity");
+		EditorApplication.SaveCurrentSceneIfUserWantsTo(); // give user a chance to save the current scene if modified
+
+		var path = EditorUtility.OpenFilePanel ("", Application.dataPath + "/Resources/vadere_output", "scenario"); //(string title, string directory, string extension)
+		var dir = Path.GetDirectoryName (path);
+		var fileName = Path.GetFileNameWithoutExtension (path);
+		var sceneName = fileName + "_scene.unity";
+
+		var createScene = true;
+
+		if (File.Exists (Application.dataPath + "/Scenes/" + sceneName))
+			createScene = EditorUtility.DisplayDialog("override scene", "A scene with the name " + sceneName + " already exists, do you want to override it?", "Yes", "No");
+
+		if (createScene) {
+			EditorApplication.NewScene();
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.transform.Rotate(0, 100, 0);
+			cube.transform.localScale = new Vector3(1.0f, 1.0f, 4.0f);
+			EditorApplication.SaveScene("Assets/Scenes/" + sceneName);
+		}
+
 	}
 
 }
-
-/* potentially useful stuff:
-
-EditorApplication.SaveCurrentSceneIfUserWantsTo(); // give user a chance to save the current scene if modified
-var triggerOverrideWarning = EditorApplication.currentScene.Equals("Assets/Scenes/MyNewScene.unity");
-var path = EditorUtility.OpenFilePanel ("", "", "scenario"); //(string title, string directory, string extension)
-Debug.Log (path);
-EditorApplication.ExecuteMenuItem("GameObject/3D Object/Cube");
-GameObject cube = GameObject.Find("Cube");
-
-https://channel9.msdn.com/Blogs/2p-start/Using-Unity-Editor-scripting-to-create-a-scene-quick-access
- */
