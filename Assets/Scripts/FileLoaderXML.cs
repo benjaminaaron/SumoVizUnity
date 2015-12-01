1deb1f39ce0e7e464ed81814721590ff7c6d2da8
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2015-12-01 11:50:04 +0100

    work on fixing trajectories import from accu:rate

--- Assets/Scripts/FileLoaderXML.cs
+++ Assets/Scripts/FileLoaderXML.cs
@@ -34,45 +34,48 @@ public class FileLoaderXML : FileLoader {
 	}
 
 	public override void loadTrajectories (string filename){
+
+		string filecontent = (Resources.Load ("accurate_output/" + filename + "_scenario") as TextAsset).text;
+
+		/*
 		// In dieser Methode die Fallunterscheidung machen zwischen alten und neuen Dateien!
 		if (!System.IO.File.Exists (filename)) {
 			Debug.LogError ("Error: File " + filename + " not found.");
 			return;
-		}
+		}*/
 		
 		XmlDocument xmlDoc = new XmlDocument();
-		xmlDoc.LoadXml(System.IO.File.ReadAllText (filename));
+		xmlDoc.LoadXml(filecontent);
 		XmlNode output = xmlDoc.SelectSingleNode ("//output");
 		if (output == null) {
 			Debug.Log("Debug: No output/ pedestrian position data found in file.");
 			return;
 		}
-		
+
 		PedestrianLoader pl = GameObject.Find("PedestrianLoader").GetComponent<PedestrianLoader>();
+
 		foreach (XmlElement floor in output.SelectNodes("floor")) {
-			if (floor.GetAttribute("csvAt") == null) {
-				using (StreamReader reader = new StreamReader(new MemoryStream(
-					Encoding.UTF8.GetBytes(floor.InnerText)))) {
-					
-					string line;
-					while((line = reader.ReadLine()) != null) {
-						string[] v = line.Split (',');
-						if(v.Length>=3) {
-							decimal time;
-							int id;
-							float x;
-							float y;
-							decimal.TryParse (v[0], out time);
-							int.TryParse(v[1], out id);
-							float.TryParse(v[2], out x);
-							float.TryParse(v[3], out y);
-							pl.addPedestrianPosition(new PedestrianPosition(id,time,x,y));
-							
-						}
-					}
+			//if (floor.GetAttribute("csvAt") == null) {
+
+				foreach (string line in floor.InnerText.ToString().Split("\n"[0])) { //TODO definitely streamreading again, but how...
+				Debug.Log (line);	
+				/*string[] v = line.Split (',');
+					if(v.Length >= 3) {
+						decimal time;
+						int id;
+						float x;
+						float y;
+						decimal.TryParse (v[0], out time);
+						int.TryParse(v[1], out id);
+						float.TryParse(v[2], out x);
+						float.TryParse(v[3], out y);
+						pl.addPedestrianPosition(new PedestrianPosition(id, time, x, y));
+					}*/
 				}
-			}
-			else {
+
+
+			//}
+			/*else { //TODO use TextAsset instead of StreamReader
 				// hier den Pfad zu den Dateien holen.... 
 				// Wie erfahre ich welche floor-ID ich brauch? Es soll ja nach floors unterschieden werden
 				// auslesen, wo die Datei liegt
@@ -101,9 +104,9 @@ public class FileLoaderXML : FileLoader {
 					float.TryParse (values[3], out y);
 					pl.addPedestrianPosition(new PedestrianPosition(id, time, x, y));
 				}
-			}
+			}*/
 		}
-		pl.createPedestrians ();	
+		pl.createPedestrians ();
 	}
 
 	public override void loadFileByPath(string path) {
