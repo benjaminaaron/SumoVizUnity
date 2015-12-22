e9e3ce459a1a4839fa37904babbed9e854174fb2
Stefan Ronczka <stefanronczka@outlook.de>
2015-12-22 10:42:03 +0100

    linkedlist try with improving from performance

--- Assets/Scripts/Pedestrian/Pedestrian.cs
+++ Assets/Scripts/Pedestrian/Pedestrian.cs
@@ -16,6 +16,8 @@ public class Pedestrian : MonoBehaviour {
 	private LinkedListNode<PedestrianPosition> iterator;
 	private LinkedListNode<PedestrianPosition> last;
 
+	private PedestrianPosition lastPos;
+
 	int id;
 	//SortedList positions = new SortedList ();
 	private LinkedList<PedestrianPosition> positions = new LinkedList<PedestrianPosition>();
@@ -45,58 +47,79 @@ public class Pedestrian : MonoBehaviour {
 		r = GetComponentInChildren<Renderer>() as Renderer;
 	
 		//set Tag of the game object in order to find gameobjects with the same tag
-		gameObject.tag = "pedestrian";	
+		//gameObject.tag = "pedestrian";	
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		//if (pc.playing) {
-			//GetComponent<Animation>().Play ();
+		/*if (pc.playing) {
+			GetComponent<Animation>().Play ();
 		/*} else {
 			GetComponent<Animation>().Stop ();
 		}*/
 
 		//int index = _getTrait(positions, pc.current_time);
-		LinkedListNode<PedestrianPosition> cur = _getTrait2(pc.current_time);
 
 
-		if(cur != null && cur != last){
+
+			
+		LinkedListNode<PedestrianPosition> cur = _getTrait2 (pc.current_time);
+
+			//Debug.Log("x:\t" +cur.Value.getX() +"y:\t"+ cur.Value.getY() + "time:\t"+cur.Value.getTime() + "id:\t" + id);
+			if (cur != null && cur != last.Previous) {
 			iterator = cur;
-		//if (index < positions.Count - 1 && index > -1){
-			active = true;
-			r.enabled = true;
-			//PedestrianPosition pos = (PedestrianPosition) positions.GetByIndex (index);
-			//PedestrianPosition pos2 = (PedestrianPosition) positions.GetByIndex (index+1);
 
-			PedestrianPosition pos = (PedestrianPosition)cur.Value;
-			PedestrianPosition pos2 = (PedestrianPosition)cur.Next.Value;
-			start = new Vector3 (pos.getX(), 0, pos.getY());
-			target = new Vector3 (pos2.getX(), 0, pos2.getY());
-			float time = pc.current_time;
-			float timeStepLength = Mathf.Clamp(pos2.getTime() - pos.getTime(), 0.1f, 50f); // We don't want to divide by zero. OTOH, this results in pedestrians never standing still.
-			float movement_percentage = (time - pos.getTime()) / timeStepLength;
-			Vector3 newPosition = Vector3.Lerp(start, target, movement_percentage);
-			Debug.Log("Pos:/tx:" + newPosition.x +"y:/t" +newPosition.z + "id:/t" + id);
+				if (r.isVisible) {
+				GetComponent<Animation>().Play ();
+					
+					//if (index < positions.Count - 1 && index > -1){
+					active = true;
+					r.enabled = true;
+					//PedestrianPosition pos = (PedestrianPosition) positions.GetByIndex (index);
+					//PedestrianPosition pos2 = (PedestrianPosition) positions.GetByIndex (index+1);
 
-			Vector3 relativePos = target - start;
-			speed = relativePos.magnitude;
+					PedestrianPosition pos = (PedestrianPosition)cur.Value;
+					PedestrianPosition pos2 = (PedestrianPosition)cur.Next.Value;
+					start = new Vector3 (pos.getX (), 0, pos.getY ());
+					target = new Vector3 (pos2.getX (), 0, pos2.getY ());
+					float time = pc.current_time;
+					float timeStepLength = Mathf.Clamp (pos2.getTime () - pos.getTime (), 0.1f, 50f); // We don't want to divide by zero. OTOH, this results in pedestrians never standing still.
+					float movement_percentage = (time - pos.getTime ()) / timeStepLength;
+					Vector3 newPosition = Vector3.Lerp (start, target, movement_percentage);
+					//Debug.Log("Pos:/tx:" + newPosition.x +"y:/t" +newPosition.z + "id:/t" + id);
 
-			GetComponent<Animation>()["MaleArm|Walking"].speed = getSpeed () / timeStepLength;
-			if (start != target) transform.rotation = Quaternion.LookRotation(relativePos);
 
-			transform.position = newPosition;
-			gameObject.hideFlags = HideFlags.None;
+					if (pos != lastPos) {
+						//lastPos = pos;
 
-		} else {
-			//currentTrait = 0;
-			iterator = positions.First;
-			active = false;
-			r.enabled = false;
-			gameObject.hideFlags = HideFlags.HideInHierarchy;
-		}
+
+						Vector3 relativePos = target - start;
+						speed = relativePos.magnitude;
+
+						GetComponent<Animation> () ["MaleArm|Walking"].speed = getSpeed () / timeStepLength;
+						if (start != target)
+							transform.rotation = Quaternion.LookRotation (relativePos);
+
+					}
+
+					transform.position = newPosition;
+					gameObject.hideFlags = HideFlags.None;
+
+				} else {
+					GetComponent<Animation> ().Stop ();
+				}
+
+			} else {
+				//currentTrait = 0;
+				iterator = positions.First;
+				active = false;
+				r.enabled = false;
+				gameObject.hideFlags = HideFlags.HideInHierarchy;
+			}
 
 	}
+
 
 	public float getSpeed() {
 		return speed;
@@ -122,7 +145,11 @@ public class Pedestrian : MonoBehaviour {
 	public LinkedListNode<PedestrianPosition> _getTrait2(float thisValue){
 		LinkedListNode<PedestrianPosition> next = iterator;
 		while ( next.Next != null ){
-			if(next.Next.Value.getTime() > thisValue){ return next.Previous;}
+			if(next.Value.getTime() > thisValue){ 
+				if(next.Previous == null) Debug.Log("prev = null");
+
+				return next.Previous;}
+			
 			next = next.Next;
 
 
