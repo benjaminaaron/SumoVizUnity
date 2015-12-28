9c902ab5a515ef29183376da14ae23b1690a8ea6
Benjamin Aaron Degenhart <b.degenhart.study@gmail.com>
2015-12-28 23:59:51 +0100

    move sphere around on tour

--- Assets/Scripts/CameraTour.cs
+++ Assets/Scripts/CameraTour.cs
@@ -3,61 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CameraTour : MonoBehaviour {
-
+	
+	private GameObject cameraObj;
 	private PlaybackControlNonGUI pc;
+	private float currentTime = 0;
 
 	private List<Vector3> waypoints = new List<Vector3>();
-	private SortedList<float, Vector3> way = new SortedList<float, Vector3> ();
-	float wayLength = 0;
+	private SortedList<float, Vector3> waypointsDist = new SortedList<float, Vector3> (); //waypoints and in addition the key is the running distance from start to end
+	float totalTourLength = 0;
+	
+	IEnumerator<KeyValuePair<float, Vector3>> enumeratorPassed;
+	IEnumerator<KeyValuePair<float, Vector3>> enumeratorNext;
+	KeyValuePair<float, Vector3> entryPassed;
+	KeyValuePair<float, Vector3> entryNext;
+	
 
 	void Start () {
 		pc = GameObject.Find ("PlaybackControl").GetComponent<PlaybackControlNonGUI> ();
+		cameraObj = GameObject.Find ("Sphere"); //TODO
 
 		//TODO read this in from .scenario file
-		waypoints.Add (new Vector3 (-10, 6, -10));
-		waypoints.Add (new Vector3 (-10, 6, 10));
-		waypoints.Add (new Vector3 (10, 6, 10));
-		waypoints.Add (new Vector3 (10, 6, -10));
-		waypoints.Add (new Vector3 (-10, 6, -10));
+		waypoints.Add (new Vector3 (-10, 1, -10));
+		waypoints.Add (new Vector3 (-10, 8, 10));
+		waypoints.Add (new Vector3 (10, 3, 10));
+		waypoints.Add (new Vector3 (10, 9, -10));
+		waypoints.Add (waypoints[0]); //close the loop
 
-		way.Add(0, waypoints [0]);
-
-		for (int i = 1; i < waypoints.Count; i++) {
-			wayLength += Vector3.Distance (waypoints [i - 1], waypoints [i]);
-			way.Add(wayLength, waypoints [i]);
+		waypointsDist.Add(0, waypoints [0]);
+		for (int i = 1; i < waypoints.Count; i ++) {
+			totalTourLength += Vector3.Distance (waypoints [i - 1], waypoints [i]);
+			waypointsDist.Add(totalTourLength, waypoints [i]);
 		}
 
-		/*
-		Debug.Log (wayLength);
-		foreach (KeyValuePair<float, Vector3> entry in way){
-			Debug.Log(entry.Value);
-			Debug.Log(entry.Key);
-		}*/
+		resetEnumerators ();
+	}
+
+	private void resetEnumerators() {
+		enumeratorPassed = waypointsDist.GetEnumerator ();
+		enumeratorNext = waypointsDist.GetEnumerator ();
+		entryPassed = enumeratorPassed.Current;
+		enumeratorNext.MoveNext (); // move this iterator one step further
+		entryNext = enumeratorNext.Current;
 	}
 	
 	void Update () {
-		float lapsedPerc = pc.current_time / pc.total_time; //from 0 to 1
-		float targetWayLength = wayLength * lapsedPerc; // from 0 to wayLength
+		if (currentTime > pc.current_time) // reset when next loop starts
+			resetEnumerators ();
 
-		IEnumerator<KeyValuePair<float, Vector3>> enumeratorPassed = way.GetEnumerator ();
-		IEnumerator<KeyValuePair<float, Vector3>> enumeratorNext = way.GetEnumerator ();
+		currentTime = pc.current_time;
+		float lapsedPerc = currentTime / pc.total_time; // from 0 to 1
+		float currentTourDist = totalTourLength * lapsedPerc; // from 0 to wayLength
 
-		KeyValuePair<float, Vector3> entryPassed = enumeratorPassed.Current;
-		enumeratorNext.MoveNext ();
-		KeyValuePair<float, Vector3> entryNext = enumeratorNext.Current;
-
-		bool cont = true;
-		while (cont){
-			cont = enumeratorPassed.MoveNext();
+		if (currentTourDist > entryNext.Key) {
+			enumeratorPassed.MoveNext();
 			enumeratorNext.MoveNext ();
 			entryPassed = enumeratorPassed.Current;
 			entryNext = enumeratorNext.Current;
-			if (entryPassed.Key > targetWayLength)
-				cont = false;
 		}
+		//Debug.Log (currentTourDist + " >> passed: " + entryPassed.Key + " / " + entryPassed.Value + ", next: " + entryNext.Key + " / " + entryNext.Value);
 
-		Debug.Log (targetWayLength + " >> passed: " + entryPassed.Key + " / " + entryPassed.Value + ", next: " + entryNext.Key + " / " + entryNext.Value);
-
+		float percBtwnWaypoints = (currentTourDist - entryPassed.Key) / (entryNext.Key - entryPassed.Key);
+		Vector3 newPos = Vector3.Lerp (entryPassed.Value, entryNext.Value, percBtwnWaypoints);
+		if(!float.IsNaN(newPos.x))
+			cameraObj.transform.position = newPos;
 	}
 
 }
