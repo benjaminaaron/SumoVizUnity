4755339b293ef6cb29ab51bca50131ddb0549ad3
Stefan Ronczka (zumbam) <stefanronczka@web.de>
2015-12-29 14:29:35 +0100

    adding solution with an trigger to reduce anims with are not can be seen

--- Assets/Scripts/Pedestrian/Pedestrian.cs
+++ Assets/Scripts/Pedestrian/Pedestrian.cs
@@ -9,12 +9,15 @@ public class Pedestrian : MonoBehaviour {
 	Vector3 target;
 	float movement_time_total;
 	float movement_time_elapsed;
+	bool animOn;
 	private float speed;
 	//to optimize the getTrait loop
 	//private int currentTrait;
 
 	private LinkedListNode<PedestrianPosition> iterator;
 	private LinkedListNode<PedestrianPosition> last;
+	public const float reducedStepTime = 0.1f;
+	//public float lastTime;
 
 	private PedestrianPosition lastPos;
 
@@ -37,7 +40,7 @@ public class Pedestrian : MonoBehaviour {
 
 		last = positions.Last;
 		iterator = positions.First;
-		gameObject.AddComponent<BoxCollider>();
+		//gameObject.AddComponent<BoxCollider>();
 		transform.Rotate (0,90,0);
 		//sets the color for the pedestrians
 		/*myColor = Color.red;
@@ -66,29 +69,41 @@ public class Pedestrian : MonoBehaviour {
 			
 		LinkedListNode<PedestrianPosition> cur = _getTrait2 (pc.current_time);
 
+
+
 			//Debug.Log("x:\t" +cur.Value.getX() +"y:\t"+ cur.Value.getY() + "time:\t"+cur.Value.getTime() + "id:\t" + id);
 			if (cur != null && cur != last.Previous) {
 			iterator = cur;
 
 				if (r.isVisible) {
-				GetComponent<Animation>().Play ();
-					
-					//if (index < positions.Count - 1 && index > -1){
-					active = true;
-					r.enabled = true;
-					//PedestrianPosition pos = (PedestrianPosition) positions.GetByIndex (index);
-					//PedestrianPosition pos2 = (PedestrianPosition) positions.GetByIndex (index+1);
 
-					PedestrianPosition pos = (PedestrianPosition)cur.Value;
-					PedestrianPosition pos2 = (PedestrianPosition)cur.Next.Value;
-					start = new Vector3 (pos.getX (), 0, pos.getY ());
-					target = new Vector3 (pos2.getX (), 0, pos2.getY ());
-					float time = pc.current_time;
-					float timeStepLength = Mathf.Clamp (pos2.getTime () - pos.getTime (), 0.1f, 50f); // We don't want to divide by zero. OTOH, this results in pedestrians never standing still.
-					float movement_percentage = (time - pos.getTime ()) / timeStepLength;
-					Vector3 newPosition = Vector3.Lerp (start, target, movement_percentage);
-					//Debug.Log("Pos:/tx:" + newPosition.x +"y:/t" +newPosition.z + "id:/t" + id);
+				//if(animOn){
+				//bool necessaryToTransform = (pc.current_time - lastTime) > reducedStepTime;
 
+
+					GetComponent<Animation>().Play ();
+							//lastTime = pc.current_time;
+							//if (index < positions.Count - 1 && index > -1){
+							active = true;
+							r.enabled = true;
+							//PedestrianPosition pos = (PedestrianPosition) positions.GetByIndex (index);
+							//PedestrianPosition pos2 = (PedestrianPosition) positions.GetByIndex (index+1);
+							
+							PedestrianPosition pos = (PedestrianPosition)cur.Value;
+							PedestrianPosition pos2 = (PedestrianPosition)cur.Next.Value;
+							start = new Vector3 (pos.getX (), 0, pos.getY ());
+							target = new Vector3 (pos2.getX (), 0, pos2.getY ());
+							float time = pc.current_time;
+							float timeStepLength = Mathf.Clamp (pos2.getTime () - pos.getTime (), 0.1f, 50f); // We don't want to divide by zero. OTOH, this results in pedestrians never standing still.
+							float movement_percentage = (time - pos.getTime ()) / timeStepLength;
+							Vector3 newPosition = Vector3.Lerp (start, target, movement_percentage);
+							//Debug.Log("Pos:/tx:" + newPosition.x +"y:/t" +newPosition.z + "id:/t" + id);
+							
+							transform.position = newPosition;
+							gameObject.hideFlags = HideFlags.None;
+
+
+				if(animOn){
 
 					if (pos != lastPos) {
 						lastPos = pos;
@@ -98,17 +113,49 @@ public class Pedestrian : MonoBehaviour {
 						speed = relativePos.magnitude;
 
 						GetComponent<Animation> () ["MaleArm|Walking"].speed = getSpeed () / timeStepLength;
-						if (start != target)
+						//if (start != target)
 							transform.rotation = Quaternion.LookRotation (relativePos);
 
 					}
 
-					transform.position = newPosition;
-					gameObject.hideFlags = HideFlags.None;
-
-				} else {
-					GetComponent<Animation> ().Stop ();
+					
 				}
+				
+				/*else if(necessaryToTransform){
+						lastTime = pc.current_time;
+						//if (index < positions.Count - 1 && index > -1){
+						active = true;
+						r.enabled = true;
+						//PedestrianPosition pos = (PedestrianPosition) positions.GetByIndex (index);
+						//PedestrianPosition pos2 = (PedestrianPosition) positions.GetByIndex (index+1);
+						
+						PedestrianPosition pos = (PedestrianPosition)cur.Value;
+						PedestrianPosition pos2 = (PedestrianPosition)cur.Next.Value;
+						start = new Vector3 (pos.getX (), 0, pos.getY ());
+						target = new Vector3 (pos2.getX (), 0, pos2.getY ());
+						float time = pc.current_time;
+						float timeStepLength = Mathf.Clamp (pos2.getTime () - pos.getTime (), 0.1f, 50f); // We don't want to divide by zero. OTOH, this results in pedestrians never standing still.
+						float movement_percentage = (time - pos.getTime ()) / timeStepLength;
+						Vector3 newPosition = Vector3.Lerp (start, target, movement_percentage);
+						//Debug.Log("Pos:/tx:" + newPosition.x +"y:/t" +newPosition.z + "id:/t" + id);
+						
+						transform.position = newPosition;
+						gameObject.hideFlags = HideFlags.None;
+						
+				}
+			*/
+
+
+				else {
+					if(GetComponent<Animation>().isPlaying)
+					GetComponent<Animation> ().Stop ();
+			}
+			}
+
+
+
+			
+			
 
 			} else {
 				//currentTrait = 0;
@@ -117,6 +164,11 @@ public class Pedestrian : MonoBehaviour {
 				r.enabled = false;
 				gameObject.hideFlags = HideFlags.HideInHierarchy;
 			}
+
+	}
+
+	private void move(){
+
 
 	}
 
@@ -146,7 +198,7 @@ public class Pedestrian : MonoBehaviour {
 		LinkedListNode<PedestrianPosition> next = iterator;
 		while ( next.Next != null ){
 			if(next.Value.getTime() > thisValue){ 
-				if(next.Previous == null) Debug.Log("prev = null");
+				//if(next.Previous == null) Debug.Log("prev = null");
 
 				return next.Previous;}
 			
@@ -180,6 +232,11 @@ public class Pedestrian : MonoBehaviour {
 
 	public bool isActive(){
 		return active;
+	}
+
+	public void setAnim(bool onoff){
+		//Debug.Log (onoff);
+		animOn = onoff;
 	}
 
 }
